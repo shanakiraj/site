f0c7645b8bf3a3feba321a8ae897958de65b4aea
shanakiraj <shan.akiraj@gmail.com>
2023-07-02 15:40:22 -0400

    changes

--- portfolio/src/app/components/Navbar.tsx
+++ portfolio/src/app/components/Navbar.tsx
@@ -1,14 +1,11 @@
-'use client';
+"use client";
 import { useState } from "react";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="p-5 mb-12 flex sticky top-0 bg-gray-100 w-full z-10">
-      <button
-        className="md:hidden block"
-        onClick={() => setIsOpen(!isOpen)}
-      >
+      <button className="md:hidden block" onClick={() => setIsOpen(!isOpen)}>
         <svg
           className="h-6 w-6 fill-current"
           xmlns="http://www.w3.org/2000/svg"
@@ -30,33 +27,32 @@ const Navbar: React.FC = () => {
       </button>
       <ul
         className={`${
-          isOpen ? 'block' : 'hidden'
+          isOpen ? "block" : "hidden"
         } md:flex md:justify-center md:items-center md:space-x-4`}
       >
         <li>
-          <a className="text-Black px-4 py-2 rounded-md " href="#">
+          <a className="text-Black px-4 py-2 rounded-md hover:text-stone-500" href="#">
             About
           </a>
         </li>
         <li>
-          <a className="text-Black px-4 py-2 rounded-md " href="#Education">
+          <a className="text-Black px-4 py-2 rounded-md hover:text-stone-500" href="#Education">
             Education
           </a>
         </li>
         <li>
-          <a className="text-black px-4 py-2 rounded-md " href="#Experience">
+          <a className="text-black px-4 py-2 rounded-md hover:text-stone-500" href="#Experience">
             Experience
           </a>
         </li>
         <li>
-          <a className="text-black px-4 py-2 rounded-md " href="#Projects">
+          <a className="text-black px-4 py-2 rounded-md hover:text-stone-500" href="#Projects">
             Projects
           </a>
         </li>
       </ul>
     </nav>
   );
-    
 };
 
 export default Navbar;
